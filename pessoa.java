816900ee0fdd7e1112e627d268e073a58c391b12
Mateus0212 <mateussilva14012006@gmail.com>
2024-12-03 13:11:53 -0300

    pessoa.java

---
+++ pessoa.java
@@ -0,0 +1,19 @@
+abstract class Pessoa {
+    private String nome;
+    private String cpf;
+
+    public Pessoa(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public abstract void mostrarDados(); // Método abstrato a ser implementado nas subclasses
+}
